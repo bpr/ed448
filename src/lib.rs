5c51632a6985c05abaf9eb156ff46b33915ccbee
Brian Rogoff <brian.rogoff@spaceandtime.io>
2024-01-24 10:59:09 -0800

    Fix lib, remove main

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,21 @@ use num_bigint::BigInt;
 use num_traits::{Zero, One};
 use std::{cmp::PartialEq, ops::{Add, Mul, Sub}};
 
+const P: &str = "fffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+const R: &str = "3fffffffffffffffffffffffffffffffffffffffffffffffffffffff7cca23e9c44edb49aed63690216cc2728dc58f552378c292ab5844f3";
+const D: &str = "-39081";
 pub struct Point {
     x: BigInt,
     y: BigInt,
 }
 
 impl Point {
-    pub fn zero() -> Self {
-        x: BigInt::from(0),
-        y: BigInt::from(1),
+    pub fn identity() -> Self {
+        Point {
+            x: BigInt::from(0),
+            y: BigInt::from(1),
+        }
     }
-
-    pub fn new(x: BigInt, y: BigInt) -> Self {
-        x,
-        y,
-    }
-}
-
-impl Add<Point> for Point {
 }
 
 #[cfg(test)]
@@ -27,8 +24,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_identity() {
+        let identity = Point::identity();
+        assert_eq!(identity.x, BigInt::from(0));
+        assert_eq!(identity.y, BigInt::from(1));
     }
 }
