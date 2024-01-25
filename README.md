752bb8489962993832db96d40413953f3b0c9e02
Brian Rogoff <brian.rogoff@spaceandtime.io>
2024-01-25 12:24:46 -0800

    Some README touchups, egcd and mod_inverse, boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# ed448
-ed448 
+<div id="top"></div>
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <h1 align="center">Ed448 Goldilocks Elliptic Curve Implemntation</h1>
+
+  <p align="center">
+    Provide a clear implementation of Ed448 suitable for educational purposes.
+    <br />
+    <a href="https://csrc.nist.gov/publications/detail/fips/186/5/final"><strong>the Ed448-Goldilocks elliptic curve was standardized in NIST FIPS 
+186-5 </strong></a>
+    <br />
+    <br />
+    <a href="https://eprint.iacr.org/2015/625">Original paper</a>
+    |
+    <a href="https://en.wikipedia.org/wiki/Curve448">Wikipedia entry</a>
+  </p>
+</div>
+
+## About the project
+The  elliptic  curve  that  will  be  implemented  is  known  as  the  Ed448-Goldilocks 
+curve (a so-called Edwards curve), defined by the following parameters: 
+• 𝑝≔2448 −2224 −1, a prime number defining the finite field 𝔽𝑝. 
+• curve equation: 𝑥2 +𝑦2 =1+𝑑𝑥2𝑦2 with 𝑑 =−39081.
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
 use num_bigint::BigInt;
 use num_traits::{Zero, One};
-use std::{cmp::PartialEq, ops::{Add, Mul, Sub}};
+use std::{cmp::PartialEq, ops::{Add, Mul, Neg}};
 
 const P: &str = "fffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 const R: &str = "3fffffffffffffffffffffffffffffffffffffffffffffffffffffff7cca23e9c44edb49aed63690216cc2728dc58f552378c292ab5844f3";
 const D: &str = "-39081";
+
+// A macro to create a BigInt from a string literal
+#[macro_export]
+macro_rules! bi {
+    ($x: literal) => {
+        $crate::bi!($x, 10)
+    };
+    ($x: literal, $base: literal) => {
+        num_bigint::BigInt::parse_bytes($x.as_bytes(), $base).unwrap()
+    }
+}
+
 pub struct Point {
     x: BigInt,
     y: BigInt,
@@ -17,6 +29,82 @@ impl Point {
             y: BigInt::from(1),
         }
     }
+
+    pub fn new(x: BigInt, y: BigInt) -> Self {
+        Point { x, y }
+    }
+
+    pub fn compute_y(x: BigInt) -> BigInt {
+        todo!("Unimplemented")
+    }
+
+    pub fn from_y_and_lsb_x (y: BigInt, lsb_x: bool) -> Self {
+        todo!("Unimplemented")
+    }
+}
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Add<Point> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Point) -> Point {
+        todo!("Unimplemented")
+    }
+}
+
+impl Neg for Point {
+    type Output = Point;
+
+    fn neg(self) -> Point {
+        if self == Point::identity() {
+            Point::identity()
+        } else {
+            Point::new(-self.x.clone(), self.y.clone())
+        }
+    }
+}
+
+impl Mul<Point> for BigInt {
+    type Output = Point;
+
+    fn mul(self, rhs: Point) -> Point {
+        todo!("Unimplemented")
+    }
+}
+
+// extended gcd: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm a helpful utility function
+pub fn extended_gcd(a: BigInt, b: BigInt) -> (BigInt, (BigInt, BigInt), (BigInt, BigInt)) {
+    let (mut old_r, mut r) = (a, b);
+    let (mut old_s, mut s) = (BigInt::from(1), BigInt::from(0));
+    let (mut old_t, mut t) = (BigInt::from(0), BigInt::from(1));
+    
+    while r != BigInt::zero() {
+        let quotient = old_r.clone() / r.clone();
+        let tmp = r.clone();
+        (old_r, r) = (r, old_r - quotient.clone() * tmp);
+        let tmp = s.clone();
+        (old_s, s) = (s, old_s - quotient.clone() * tmp);
+        let tmp = t.clone();
+        (old_t, t) = (t, old_t - quotient.clone() * tmp);
+    }
+    (old_r, (old_s, old_t), (s, t))
+}
+
+pub fn mod_inverse(a: BigInt, m: BigInt) -> BigInt {
+    let (gcd, (s, _), _) = extended_gcd(a.clone(), m.clone());
+    if gcd != BigInt::from(1) {
+        panic!("{} and {} are not coprime", a, m);
+    }
+    if s < BigInt::zero() {
+        m + s
+    } else {
+        s
+    }
 }
 
 #[cfg(test)]
@@ -28,5 +116,19 @@ mod tests {
         let identity = Point::identity();
         assert_eq!(identity.x, BigInt::from(0));
         assert_eq!(identity.y, BigInt::from(1));
+    }
+
+    #[test]
+    fn simple_egcd_test() {
+        let (gcd, (s, t), (old_s, old_t)) = extended_gcd(bi!("240"), bi!("46"));
+        assert_eq!([gcd, s, t, old_s, old_t],
+            [bi!("2"), bi!("-9"), bi!("47"), bi!("23"), bi!("-120")]);
+    }
+
+    #[test]
+    fn mod_inverse_test() {
+        assert_eq!(mod_inverse(bi!("3"), bi!("11")), bi!("4"));
+        assert_eq!(mod_inverse(bi!("5"), bi!("11")), bi!("9"));
+        assert_eq!(mod_inverse(bi!("7"), bi!("11")), bi!("8"));
     }
 }
